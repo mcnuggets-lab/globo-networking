a5cdb578221998edff1ac2b6d5336a952a7def30
Kit-Ho Mak <mcnuggets23@gmail.com>
2024-09-14 11:19:47 +0800

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,46 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "aws_region" {
+  type        = string
+  description = "(Optional) AWS Region to use. Default: us-east-1"
+  default     = "us-east-1"
+}
+
+variable "aws_access_key_id" {
+  type        = string
+  description = "AWS_ACCESS_KEY_ID"
+}
+
+variable "aws_secret_access_key" {
+  type        = string
+  description = "AWS_SECRET_ACCESS_KEY"
+}
+
+variable "aws_session_token" {
+  type        = string
+  description = "AWS_SESSION_TOKEN"
+}
+
+variable "prefix" {
+  type        = string
+  description = "(Optional) Prefix to use for all resources in this module. Default: globo-dev"
+  default     = "globo-dev"
+}
+
+variable "cidr_block" {
+  type        = string
+  description = "(Optional) The CIDR block for the VPC. Default:10.42.0.0/16"
+  default     = "10.42.0.0/16"
+}
+
+variable "public_subnets" {
+  type        = map(string)
+  description = "(Optional) Map of public subnets to create with CIDR blocks. Key will be used as subnet name with prefix. Default: {subnet-1 ="
+  default = {
+    public-1 = "10.42.10.0/24"
+    public-2 = "10.42.11.0/24"
+  }
+}
+
